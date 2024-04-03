0ecfe7c8c0d6970bb936d1a64d8dfa2849dd46ee
Patries <patriesf@gmail.com>
2024-04-03 16:59:28 +0100

    Work in progress. Fetched pet data

--- main.js
+++ main.js
@@ -7,5 +7,11 @@ async function start() {
 start()
 
 
-
-
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(() => {
+    console.log("hello")
+  })
+}
+petsArea()
